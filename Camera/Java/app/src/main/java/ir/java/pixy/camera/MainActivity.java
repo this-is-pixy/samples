79779e533ac57fd70c6b964a0052987e0646d54f
Pixy <4othereg@gmail.com>
2020-07-18 10:16:18 +0430

    Camera Java

---
+++ Camera/Java/app/src/main/java/ir/java/pixy/camera/MainActivity.java
@@ -0,0 +1,46 @@
+package ir.java.pixy.camera;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static int CAMERA_REQUEST = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button camera = findViewById(R.id.camera);
+        camera.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.camera) {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Uri imageUri = data.getData();
+            if(imageUri != null) {
+                // do something with image
+            }
+        }
+    }
+}
